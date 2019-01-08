b36d69809a87c78f2cdf56f754269250f33eefdd
tbreuss <thomasbreuss@gmx.ch>
2019-01-08 22:10:08 +0100

    Synchronize to Herbie 2.0

--- textile.php
+++ textile.php
@@ -2,23 +2,31 @@
 
 namespace herbie\plugin\textile;
 
+use Herbie\Config;
+use Herbie\PluginInterface;
 use Herbie\StringValue;
 use Zend\EventManager\EventInterface;
 use Zend\EventManager\EventManagerInterface;
 
-class TextilePlugin extends \Herbie\Plugin
+class TextilePlugin implements PluginInterface
 {
+    private $config;
+
+    public function __construct(Config $config)
+    {
+        $this->config = $config;
+    }
+
     /**
      * @param EventManagerInterface $events
      * @param int $priority
      */
     public function attach(EventManagerInterface $events, $priority = 1): void
     {
-        $config = $this->getConfig();
-        if ((bool)$config->get('plugins.config.textile.twig', false)) {
+        if ((bool)$this->config->get('plugins.config.textile.twig', false)) {
             $events->attach('onTwigInitialized', [$this, 'onTwigInitialized'], $priority);
         }
-        if ((bool)$config->get('plugins.config.textile.shortcode', true)) {
+        if ((bool)$this->config->get('plugins.config.textile.shortcode', true)) {
             $events->attach('onShortcodeInitialized', [$this, 'onShortcodeInitialized'], $priority);
         }
         $events->attach('onRenderContent', [$this, 'onRenderContent'], $priority);
@@ -55,6 +63,7 @@ class TextilePlugin extends \Herbie\Plugin
      */
     public function onRenderContent(EventInterface $event)
     {
+        #echo __METHOD__ . "<br>";
         if (!in_array($event->getParam('format'), ['textile'])) {
             return;
         }
